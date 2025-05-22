d51266457ba4bbdf7a0c2669215c718b3d2b00bb
Richard Kadey <richardkadey@gmail.com>
2025-05-22 22:33:56 +0000

    data setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wine_project",
+    version="0.0.0",
+    author="Richard",
+    author_email="richardkadey@gmail.com",
+    packages=find_packages()
+)
