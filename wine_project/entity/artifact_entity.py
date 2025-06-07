baa6ecd38babc3fa050c64b33c4955a57b2889ce
Richard Kadey <richardkadey@gmail.com>
2025-06-07 22:09:11 +0000

    data validation code added

--- wine_project/entity/artifact_entity.py
+++ wine_project/entity/artifact_entity.py
@@ -6,3 +6,9 @@ class DataIngestionArtifact:
     feature_store_file_path:str
     trained_file_path:str 
     test_file_path:str 
+
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    drift_report_file_path: str
