8856d3327ff2daed51b1e20259ccb9301fe2dc8e
Richard Kadey <richardkadey@gmail.com>
2025-06-08 23:25:10 +0000

    training pipeline updated with data validation

--- wine_project/pipeline/traning_pipeline.py
+++ wine_project/pipeline/traning_pipeline.py
@@ -1,6 +1,7 @@
-from wine_project.entity.config_entity import DataIngestionConfig
-from wine_project.entity.artifact_entity import DataIngestionArtifact
+from wine_project.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+from wine_project.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact)
 from wine_project.components.data_ingestion import DataIngestion
+from wine_project.components.data_validation import DataValidation
 from wine_project.constants import *
 from wine_project.exception import WineException
 from wine_project.logger import logging
@@ -9,7 +10,7 @@ import sys
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-      
+        self.data_validation_config = DataValidationConfig()
 
 
     
@@ -30,6 +31,26 @@ class TrainPipeline:
         except Exception as e:
             raise WineException(e, sys) from e
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+            return data_validation_artifact
+
+        except Exception as e:
+            raise WineException(e, sys) from e
+        
 
 
     def run_pipeline(self) -> None:
@@ -38,6 +59,7 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_ingest_data()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
         
         except Exception as e:
             raise WineException(e, sys)
